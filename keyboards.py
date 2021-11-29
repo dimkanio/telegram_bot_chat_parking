bbdac912964e4fd2c48eabdc157ed43bd6017d90
dimkanio <dimkaliamg@gmail.com>
2021-11-30 02:16:58 +0300

    add anon

--- keyboards.py
+++ keyboards.py
@@ -25,7 +25,7 @@ cancel_direct_dialog = InlineKeyboardButton('❌ Не отвечать', callbac
 reply_anon_btn = InlineKeyboardButton('🕶 Ответить анонимно', callback_data='reply_anonym_btn')
 reply_direct_btn = InlineKeyboardButton('👓 Ответить', callback_data='reply_direct_btn')
 message_direct_dialog_btn_markup = InlineKeyboardMarkup().add(reply_direct_btn).add(cancel_direct_dialog)
-message_direct_dialog_btn_markup = InlineKeyboardMarkup().add(reply_anon_btn).add(cancel_direct_dialog)
+message_anon_dialog_btn_markup = InlineKeyboardMarkup().add(reply_anon_btn).add(cancel_direct_dialog)
 
 ############################## SETTINGS BTNS ################################
 mm_settings_btn = InlineKeyboardButton('🅿️ Добавить/Изменить машиноместа', callback_data='mm_settings_btn')
