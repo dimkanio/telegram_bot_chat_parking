65cdaf920056942d43161dd1d70daeab0fac5a95
dimkanio <dimkaliamg@gmail.com>
2021-12-08 04:13:12 +0300

    fix

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php 
+    echo '<pre>';
     echo "Карта паркинга";
+    echo '</pre>';
+
     if(file_exists("map.html")) {
+        echo '<pre>';
         include_once("map.html"); 
+        echo '</pre>';
     }
     else {
+        echo '<pre>';
         print "Карта не подгрузидась";
+        echo '</pre>';
     }
 
     
