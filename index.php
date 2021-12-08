17c97a5b49d90687f5feec046bcb62a42ee52f78
dimkanio <dimkaliamg@gmail.com>
2021-12-09 01:31:42 +0300

    add page php

--- index.php
+++ index.php
@@ -1,18 +1,26 @@
-<?php 
-    echo '<pre>';
-    echo "Карта паркинга";
-    echo '</pre>';
+<html>
+ <head>
+  <title>Карта паркинга</title>
+ </head>
+ <body>
+ <?php 
+    $db_url = getenv("DATABASE_URL");
+    $dbconn = pg_connect($db_url);
 
-    if(file_exists("map.html")) {
-        echo '<pre>';
-        include_once("map.html"); 
-        echo '</pre>';
-    }
-    else {
-        echo '<pre>';
-        print "Карта не подгрузидась";
-        echo '</pre>';
-    }
+    if($dbconn) {
+        $selectSql = 'SELECT page_html FROM html WHERE num = 100';
+        $result =  pg_query($dbconn, $selectSql);
 
-    
+        while ($row = pg_fetch_row($result)) {
+            echo $row[0];
+        }
+
+    } else {
+        echo '<pre>';
+        echo "Карта паркинга не подгрузилась.";
+        echo '</pre>';
+    };
 ?>
+ </body>
+</html>
+
